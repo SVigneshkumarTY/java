e48126008482a1de81322d3aedae14f6de081ca1
shitaldhurat3 <shital.d@testyantra.in>
2019-10-24 17:04:23 +0530

    added

---
+++ library_management/src/main/java/com/tyss/library/management/librarymanagement/AppTest.java
@@ -0,0 +1,28 @@
+package com.tyss.library.management.librarymanagement;
+
+import java.awt.print.Book;
+
+import org.apache.catalina.User;
+
+public class AppTest {
+	    private Book book1;
+	    private Book book2;
+	    private Book book3;
+
+	    private User user1;
+	    
+	    public void deleteBookByIDTest() {
+	       
+	    }
+
+	  //  private UserBook userBook1
+	   // @Before
+//	    public void setUp() {
+//	        book1 = new Book("1111111111111", "Author", "Title", "12:34:56", "Publisher", "2016", "MLK", 5, "Available", "test,book", null);
+//	        book2 = new Book("2222222222222", "Author", "Title", "12:34:56", "Publisher", "2016", "MLK", 5, "Available", "test,book", null);
+//	        book3 = new Book("3333333333333", "Author", "Title", "12:34:56", "Publisher", "2016", "MLK", 1, "Available", "test,book", null);
+//	        user1 = new User(123456, "user@gmail.com", "password", "ROLE_USER", true);
+
+	     //   userBook1 = new UserBook(book1, user1, LocalDateTime.now(), 0);
+	   //
+}
